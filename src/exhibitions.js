b0211ed440771b8887f0fd437a11b5ab9ff4afb7
蔣 <novemba32@gmail.com>
2026-06-02 18:18:16 +0800

    心

--- src/exhibitions.js
+++ src/exhibitions.js
@@ -1,1 +1,222 @@
-Unsupported Media Type
+export const EXHIBITIONS = [
+  {
+    id: "bronze",
+    title: "常設展｜青銅器廳",
+    location: "第一展覽館 1F 101",
+    floor: "1F",
+    rooms: ["101"],
+    duration: 18,
+    tags: ["青銅器", "歷史", "禮制"],
+    crowd: "中",
+    image: "/exhibitions/bronze.jpg",
+    description: "本廳精選商周至戰國青銅器。"
+  },
+
+  {
+    id: "dragon-sutra",
+    title: "特展｜《龍藏經》：皇權・信仰・藝術的盛世交響",
+    location: "第一展覽館 103、104",
+    floor: "1F",
+    rooms: ["103", "104"],
+    duration: 60,
+    tags: ["善本書籍", "器物"],
+    crowd: "中",
+    image: "/exhibitions/20260509.jpg",
+    description: "以清宮《龍藏經》為核心，呈現皇權、信仰與工藝的結合。"
+  },
+
+  {
+    id: "qianlong",
+    title: "特展｜真假乾隆",
+    location: "第一展覽館 202、204、206",
+    floor: "2F",
+    rooms: ["202", "204", "206"],
+    duration: 90,
+    tags: ["書法"],
+    crowd: "中",
+    image: "/exhibitions/20260421.jpg",
+    description: "探討乾隆御筆與代筆書家的差異與歷史脈絡。"
+  },
+
+  {
+    id: "national-treasure",
+    title: "特展｜國寶聚焦 2026-II",
+    location: "第一展覽館 208",
+    floor: "2F",
+    rooms: ["208"],
+    duration: 15,
+    tags: ["書畫"],
+    crowd: "中",
+    image: "/exhibitions/202604142.jpg",
+    description: "每季聚焦一件國寶級作品。"
+  },
+
+  {
+    id: "chu-ci",
+    title: "特展｜獨騷－楚辭文化意象與龍舟",
+    location: "第一展覽館 210、212",
+    floor: "2F",
+    rooms: ["210", "212"],
+    duration: 90,
+    tags: ["繪畫"],
+    crowd: "中",
+    image: "/exhibitions/202604141.jpg",
+    description: "從屈原與《楚辭》探索文化意象。"
+  },
+
+  {
+    id: "beasts",
+    title: "特展｜神獸再現",
+    location: "第一展覽館 105、107",
+    floor: "1F",
+    rooms: ["105", "107"],
+    duration: 60,
+    tags: ["書法", "繪畫", "器物"],
+    crowd: "高",
+    image: "/exhibitions/20260320.jpg",
+    description: "探索神獸文化與東亞想像力。"
+  },
+
+  {
+    id: "dream-red",
+    title: "特展｜看得見的紅樓夢",
+    location: "第一展覽館 203",
+    floor: "2F",
+    rooms: ["203"],
+    duration: 45,
+    tags: ["陶瓷", "玉器", "珍玩"],
+    crowd: "高",
+    image: "/exhibitions/20240517.jpg",
+    description: "從文物重新閱讀《紅樓夢》。"
+  },
+
+  {
+    id: "weapons",
+    title: "特展｜祀與戎：古代兵器攻略",
+    location: "第一展覽館 304",
+    floor: "3F",
+    rooms: ["304"],
+    duration: 45,
+    tags: ["玉器", "銅器"],
+    crowd: "中",
+    image: "/exhibitions/20231201.jpg",
+    description: "從石器、青銅到鐵器的發展歷程。"
+  },
+
+  {
+    id: "inkstone",
+    title: "特展｜愛硯成痴",
+    location: "第一展覽館 303",
+    floor: "3F",
+    rooms: ["303"],
+    duration: 45,
+    tags: ["器物"],
+    crowd: "低",
+    image: "/exhibitions/20230928.jpg",
+    description: "欣賞歷代名硯與文人雅趣。"
+  },
+
+  {
+    id: "rococo",
+    title: "特展｜華麗魔法屋",
+    location: "第一展覽館 300、301",
+    floor: "3F",
+    rooms: ["300", "301"],
+    duration: 45,
+    tags: ["器物"],
+    crowd: "中",
+    image: "/exhibitions/20211224.jpg",
+    description: "故宮收藏的歐洲洛可可珍品。"
+  },
+
+  {
+    id: "palace-life",
+    title: "特展｜貴冑榮華",
+    location: "第一展覽館 108",
+    floor: "1F",
+    rooms: ["108"],
+    duration: 30,
+    tags: ["珍玩"],
+    crowd: "中",
+    image: "/exhibitions/20200501.jpg",
+    description: "清代宮廷生活與家具文化。"
+  },
+
+  {
+    id: "treasures",
+    title: "常設展｜集瓊藻",
+    location: "第一展覽館 106",
+    floor: "1F",
+    rooms: ["106"],
+    duration: 75,
+    tags: ["珍玩"],
+    crowd: "中",
+    image: "/exhibitions/8015630.jpg",
+    description: "院藏珍玩精華展。"
+  },
+
+  {
+    id: "jade",
+    title: "常設展｜敬天格物",
+    location: "第一展覽館 306、308",
+    floor: "3F",
+    rooms: ["306", "308"],
+    duration: 60,
+    tags: ["玉器"],
+    crowd: "中",
+    image: "/exhibitions/8000140.jpg",
+    description: "中國玉器八千年發展史。"
+  },
+
+  {
+    id: "bronze-gallery",
+    title: "常設展｜吉金耀采",
+    location: "第一展覽館 305、307",
+    floor: "3F",
+    rooms: ["305", "307"],
+    duration: 60,
+    tags: ["銅器"],
+    crowd: "中",
+    image: "/exhibitions/8000126.jpg",
+    description: "院藏銅器精華展。"
+  },
+
+  {
+    id: "ceramics",
+    title: "常設展｜摶泥幻化",
+    location: "第一展覽館 201、205、207",
+    floor: "2F",
+    rooms: ["201", "205", "207"],
+    duration: 60,
+    tags: ["陶瓷"],
+    crowd: "中",
+    image: "/exhibitions/8000120.jpg",
+    description: "中國陶瓷發展史。"
+  },
+
+  {
+    id: "buddha",
+    title: "常設展｜慈悲與智慧",
+    location: "第一展覽館 101",
+    floor: "1F",
+    rooms: ["101"],
+    duration: 30,
+    tags: ["宗教雕塑"],
+    crowd: "中",
+    image: "/exhibitions/8000033.jpg",
+    description: "佛教雕塑藝術。"
+  },
+
+  {
+    id: "kids",
+    title: "兒童學藝中心",
+    location: "第一展覽館 B1",
+    floor: "B1",
+    rooms: ["B1"],
+    duration: 40,
+    tags: ["數位新媒體"],
+    crowd: "高",
+    image: "/exhibitions/20200701.jpg",
+    description: "親子互動探索空間。"
+  }
+];
